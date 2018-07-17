482d2c1dd308be71452f4704d01668319b48da9b
mmalarz <mmalarz@protonmail.com>
2018-07-17 20:34:28 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webcrawler',
+    version='0.1',
+    description='Webcrawler that gathers links from website',
+    url='https://github.com/mmalarz/webcrawler',
+    license='LICENSE',
+    packaages=find_packages(),
+    install_requires=[
+        'beautifulsoup4 >= 4.6.0',
+        'lxml >= 4.2.1',
+        'requestes >= 2.18.4',
+    ],
+    zip_safe=False
+)
+
